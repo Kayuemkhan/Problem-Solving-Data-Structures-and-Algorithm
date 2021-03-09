d9ce064f13e37149a884b6e21db5ed0ac379af3b
Kayuemkhan <abdulkayuem007@gmail.com>
2021-03-09 17:59:18 +0600

    initial

---
+++ DS/Array.java
@@ -0,0 +1,16 @@
+package DS;
+
+import java.util.Scanner;
+
+public class Array {
+    public static void main(String[] args) {
+        // Creating an Array
+        int arr[] = new int[10];
+        // pusing new Items
+        for(int i =0;i<5;i++){
+            Scanner scanner  = new Scanner(System.in);
+            arr[i] = scanner.nextInt();
+        }
+
+    }
+}
