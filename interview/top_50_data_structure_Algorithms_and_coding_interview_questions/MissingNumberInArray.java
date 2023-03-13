814b100bb25fe291a85f0b406045f7dd684a5612
Abdul Kayuem <abdulkayuem007@gmail.com>
2023-03-13 11:49:12 +0600

    Pattern_3_Right_Angled_Number_Pyramid

--- interview/top_50_data_structure_Algorithms_and_coding_interview_questions/MissingNumberInArray.java
+++ interview/top_50_data_structure_Algorithms_and_coding_interview_questions/MissingNumberInArray.java
@@ -12,15 +12,15 @@ public class MissingNumberInArray {
  
     public static void main(String args[]) {
 
-        // one missing number
-        printMissingNumber(new int[]{1, 2, 3, 4, 6}, 6);
- 
-        // two missing number
-        printMissingNumber(new int[]{1, 2, 3, 4, 6, 7, 9, 8, 10}, 10);
- 
-        // three missing number
-        printMissingNumber(new int[]{1, 2, 3, 4, 6, 9, 8}, 10);
- 
+//        // one missing number
+//        printMissingNumber(new int[]{1, 2, 3, 4, 6}, 6);
+//
+//        // two missing number
+//        printMissingNumber(new int[]{1, 2, 3, 4, 6, 7, 9, 8, 10}, 10);
+//
+//        // three missing number
+//        printMissingNumber(new int[]{1, 2, 3, 4, 6, 9, 8}, 10);
+//
         // four missing number
         printMissingNumber(new int[]{1, 2, 3, 4, 9, 8}, 10);
  
@@ -39,6 +39,7 @@ public class MissingNumberInArray {
         BitSet bitSet = new BitSet(count);
  
         for (int number : numbers) {
+            System.out.println("number "+number);
             bitSet.set(number - 1);
         }
  
