8de1328872be44dc3ac29cdf7c1b733269f3411b
Abdul Kayuem <abdulkayuem007@gmail.com>
2023-03-14 11:44:39 +0600

    Star Pyramid

--- basic/patterns/Pattern_8_Inverted_Star_Pyramid.java
+++ basic/patterns/Pattern_8_Inverted_Star_Pyramid.java
@@ -4,20 +4,21 @@ public class Pattern_8_Inverted_Star_Pyramid {
     public static void main(String[] args) {
         int n =6;
 
-        for (int i = n; i > 0; i--) {
+        for (int i = 0; i < n; i++){
 
-
-            for ( int j = 0; j < n-i-1 ; j++ ) {
+            for (int j =0; j<i; j++)
+            {
                 System.out.print(" ");
             }
 
 
-            for(int j=0;j< 2*i+1;j++){
+            for(int j=0;j< 2*n -(2*i +1);j++){
 
                 System.out.print("*");
             }
 
-            for ( int j = 0; j < n-i-1 ; j++ ) {
+            for (int j =0; j<i; j++)
+            {
                 System.out.print(" ");
             }
 
