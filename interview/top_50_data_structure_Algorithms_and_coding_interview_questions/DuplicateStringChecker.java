7dd742bac420588619314e8ec3269e0c47c24f51
Kayuemkhan <abdulkayuem007@gmail.com>
2023-09-21 13:43:55 +0600

    add some more elements

---
+++ interview/top_50_data_structure_Algorithms_and_coding_interview_questions/DuplicateStringChecker.java
@@ -0,0 +1,30 @@
+package interview.top_50_data_structure_Algorithms_and_coding_interview_questions;
+
+public class DuplicateStringChecker {
+
+    public static boolean bruteforce(String[] input) {
+        for (int i = 0; i < input.length; i++) {
+            for (int j = 0; j < input.length; j++) {
+                if (i != j && input[i].equals(input[j])) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        // Create an array of strings to check for duplicates
+        String[] inputArray = { "apple", "banana", "cherry", "apple", "date" };
+
+        // Call the bruteforce method to check for duplicates
+        boolean hasDuplicates = bruteforce(inputArray);
+
+        // Print the result
+        if (hasDuplicates) {
+            System.out.println("The array contains duplicate strings.");
+        } else {
+            System.out.println("The array does not contain duplicate strings.");
+        }
+    }
+}
