34537df28573890709b8898e51b404c47fb510ef
Kayuemkhan <abdulkayuem007@gmail.com>
2023-09-21 10:43:05 +0600

    primetest

--- basicDS/Modulus.java
+++ basicDS/Modulus.java
@@ -3,23 +3,19 @@ package basicDS;
 import java.util.Scanner;
 
 public class Modulus {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        int n = scanner.nextInt();
-
-        for (int i = 0; i <n ; i++) {
-            int input = scanner.nextInt();
-            if ( input == 0 ){
-                System.out.println("1");
-            }
-            else {
-                long calFact =1;
-                for (int j = 1; j <input; j++) {
-                    calFact += j * calFact;
-                }
-                System.out.println(calFact);
+    public static void main(String args[]) {
+        Scanner scnr = new Scanner(System.in);
+        int number;
+        number = scnr.nextInt();
+        System.out.println(isPrime(number));
+    }
+    public static String isPrime(int number) {
+        int sqrt = (int) Math.sqrt(number) + 1;
+        for (int i = 2; i < sqrt; i++) {
+            if (number % i == 0) {
+                return "NO";
             }
         }
+        return "YES";
     }
 }
