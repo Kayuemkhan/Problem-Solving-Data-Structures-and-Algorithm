be0d0b2a9acac85d88ef2371d0f2d4c98ee08b37
Kayuemkhan <abdulkayuem007@gmail.com>
2023-12-24 18:11:52 +0600

    add all

--- algorithms/basic/sort/SelectionSort.java
+++ algorithms/basic/sort/SelectionSort.java
@@ -3,7 +3,7 @@ package algorithms.basic.sort;
 public class SelectionSort {
 
     public static void main(String[] args) {
-        int arr[] = {64,25,12,22,12,11};
+        int[] arr = {64,25,12,22,12,11};
         // sorting
         for(int i =0;i<arr.length;i++){
 
