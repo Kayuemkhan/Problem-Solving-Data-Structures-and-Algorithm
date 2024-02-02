b3d8221b6b53e45478d93c2468280be60d091e64
Kayuemkhan <abdulkayuem007@gmail.com>
2024-02-02 20:23:33 +0600

    add all

--- IntegerSet.java
+++ IntegerSet.java
@@ -1,76 +1,27 @@
+import java.math.BigInteger;
+import java.util.Arrays;
+
 public class IntegerSet {
 
-    private final boolean[] integerSet;
-
-    // no argument constructor
-    public IntegerSet() {
-       // Array size for integers 0-100
-        integerSet = new boolean[101];
-    }
-
-    public static IntegerSet union(IntegerSet set1, IntegerSet set2) {
-        IntegerSet resultSet = new IntegerSet();
-        for (int i = 0; i < resultSet.integerSet.length; i++) {
-            resultSet.integerSet[i] = set1.integerSet[i] || set2.integerSet[i];
-        }
-        return resultSet;
-    }
-
-    public static IntegerSet intersection(IntegerSet set1, IntegerSet set2) {
-        IntegerSet resultSet = new IntegerSet();
-        for (int i = 0; i < resultSet.integerSet.length; i++) {
-            resultSet.integerSet[i] = set1.integerSet[i] && set2.integerSet[i];
-        }
-        return resultSet;
-    }
-
-    public void insertElement(int k) {
-        if (k >= 0 && k <= 100) {
-            integerSet[k] = true;
-        }
-    }
-
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
-        boolean isEmpty = true;
-        for (int i = 0; i < integerSet.length; i++) {
-            if (integerSet[i]) {
-                sb.append(i).append(" ");
-                isEmpty = false;
-            }
-        }
-        if (isEmpty) {
-            return "---";
-        } else {
-            return sb.toString().trim();
-        }
-    }
-
-    public boolean isEqualTo(IntegerSet otherSet) {
-        for (int i = 0; i < integerSet.length; i++) {
-            if (integerSet[i] != otherSet.integerSet[i]) {return false;}
-        }
-        return true;
-    }
-
     public static void main(String[] args) {
-        IntegerSet integerSet = new IntegerSet();
-        IntegerSet integerSet1 = new IntegerSet();
+        int[] nums = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
 
-        integerSet.insertElement(5);
-        integerSet.insertElement(10);
-        integerSet.insertElement(20);
-        integerSet1.insertElement(10);
-        integerSet1.insertElement(15);
-        integerSet1.insertElement(20);
+        StringBuilder sum = new StringBuilder();
 
-        System.out.println("Set 1: " + integerSet);
-        System.out.println("Set 2: " + integerSet1);
-        IntegerSet unionSet = IntegerSet.union(integerSet, integerSet1);
-        System.out.println("Union of integerSet and integerSet1: " + unionSet);
+        for (int num : nums) {
+            sum.append(num);
+        }
 
-        IntegerSet intersectionSet = IntegerSet.intersection(integerSet, integerSet1);
-        System.out.println("Intersection of integerSet and integerSet1: " + intersectionSet);
-        System.out.println("Is integerSet equal to integerSet1? " + integerSet.isEqualTo(integerSet1));
+        BigInteger bigSum = new BigInteger(sum.toString()).add(BigInteger.ONE);
+
+        String sumString = bigSum.toString();
+
+        int[] output = new int[sumString.length()];
+
+        for (int i = 0; i < sumString.length(); i++) {
+            output[i] = Character.getNumericValue(sumString.charAt(i));
+        }
+
+        System.out.println(Arrays.toString(output));
     }
 }
