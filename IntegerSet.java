71d8f82b6ba991a50bf1c41e398796284cf88ecf
Kayuemkhan <abdulkayuem007@gmail.com>
2024-02-04 11:39:51 +0600

    add all

--- IntegerSet.java
+++ IntegerSet.java
@@ -1,21 +1,47 @@
 import java.math.BigInteger;
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class IntegerSet {
 
     public static void main(String[] args) {
-        int[] nums = {0,1,2,2,3,0,4,2};
-        Arrays.sort(nums);
-        int val = 2;
-        System.out.println(Arrays.toString(nums));
-        int j =0;
-        for (int i = 0; i < nums.length; i++) {
-            if(nums[i] != val){
-                nums[j] = nums[i];
-                j++;
+        int[] nums = {0,2,3,4,6,8,9};
+
+        int start = nums[0];
+        int end = nums[0];
+        ArrayList < String > x = new ArrayList<>();
+        for (int i = 1; i < nums.length; i++) {
+
+            if(nums[i]-1 == nums[i-1]){
+                end = nums[i];
             }
+            else {
+                if(start == end){
+                    x.add(String.valueOf(end));
+                }
+                else {
+                    x.add(start+"->"+end);
+                    start = nums[i];
+                    end = nums[i];
+                }
+
+                System.out.println("start");
+                System.out.println(start);
+                System.out.println("end");
+                System.out.println(end);
+
+            }
+            if(i == nums.length-1){
+                if(start == end){
+                    x.add(String.valueOf(end));
+                }
+                else {
+                    x.add(start+"->"+end);
+                }
+            }
+
         }
-        System.out.println(Arrays.toString(nums));
-        System.out.println(j);
+        System.out.println(x);
+
     }
 }
