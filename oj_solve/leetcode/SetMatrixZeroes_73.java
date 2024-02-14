45b85941140774bc1c9c0d3d7fdcef5b9e675c60
Kayuemkhan <abdulkayuem007@gmail.com>
2024-02-14 11:51:11 +0600

    setmatrix zeo

--- oj_solve/leetcode/SetMatrixZeroes_73.java
+++ oj_solve/leetcode/SetMatrixZeroes_73.java
@@ -1,7 +1,8 @@
 package oj_solve.leetcode;
 
-import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
+import java.util.Set;
 
 public class SetMatrixZeroes_73 {
     public static void main(String[] args) {
@@ -10,38 +11,42 @@ public class SetMatrixZeroes_73 {
                 {3,4,5,2},
                 {1,3,1,5}
         };
-        HashMap<String,Integer> hashMap = new HashMap<>();
-
+        Set<Integer> rows = new HashSet<>();
+        Set<Integer> cols = new HashSet<>();
         for (int i = 0; i < prevArr.length; i++) {
             for (int j = 0; j < prevArr[i].length; j++) {
                 if(prevArr[i][j] == 0){
-                    hashMap.put("i",i);
-                    hashMap.put("j",j);
-
+                    rows.add(i);
+                    cols.add(j);
                 }
             }
         }
+        Iterator<Integer> itr = rows.iterator();
+        Iterator<Integer> itr2 = cols.iterator();
 
-        for(String i : hashMap.keySet()){
-            System.out.println("key: " + i + " value: " + hashMap.get(i));
+
+//        while (itr.hasNext()) {
+//            System.out.println(itr.next());
+//        }
+        while (itr2.hasNext()) {
+            System.out.println(itr2.next());
         }
-
-
 
         for (int i = 0; i < prevArr.length; i++) {
             for (int j = 0; j < prevArr[i].length; j++) {
-                if( hashMap.get("i") == i|| hashMap.get("j") ==i || hashMap.get("j") == j) {
+                if (rows.contains(i) || cols.contains(j)) {
                     prevArr[i][j] = 0;
                 }
             }
         }
 
-        for (int i = 0; i < prevArr.length; i++) {
-            for (int j = 0; j < prevArr[i].length; j++) {
-                System.out.print(prevArr[i][j]);
-            }
-            System.out.println();
-        }
+//        for (int i = 0; i < prevArr.length; i++) {
+//            for (int j = 0; j < prevArr[i].length; j++) {
+//                System.out.print(prevArr[i][j]+" ");
+//            }
+//            System.out.println();
+//        }
 
     }
 }
+
