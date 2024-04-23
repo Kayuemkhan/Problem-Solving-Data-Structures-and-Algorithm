2b40a2960144c75d9d85800ca26d60846d681fcd
Kayuemkhan <abdulkayuem007@gmail.com>
2024-04-23 11:54:54 +0600

    hashing added

--- oj_solve/leetcode/Isomorphic_Strings_205.java
+++ oj_solve/leetcode/Isomorphic_Strings_205.java
@@ -2,6 +2,13 @@ package oj_solve.leetcode;
 
 public class Isomorphic_Strings_205 {
     public static void main(String[] args) {
+        String str1 = "abc";
+        int str1ToInt = str1.hashCode();
+        int str123ToInt = str1.hashCode();
+        System.out.println(str123ToInt);
 
+        String str2 = "abcd";
+        int str2ToInt = str2.hashCode();
+        System.out.println(str2ToInt);
     }
 }
