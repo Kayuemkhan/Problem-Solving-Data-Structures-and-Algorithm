510f68d4a625a6924aef3f5f4cccfeaf97c3aa23
Kayuemkhan <abdulkayuem007@gmail.com>
2024-08-12 08:23:49 +0600

    custom stack added by array

--- dataStructures/stack/CustomStack.java
+++ dataStructures/stack/CustomStack.java
@@ -4,7 +4,6 @@ public class CustomStack {
     protected int[] data;
     private static final int DEFAULT_SIZE = 10;
     int ptr = -1;
-
     public CustomStack(){
         this(DEFAULT_SIZE);
     }
