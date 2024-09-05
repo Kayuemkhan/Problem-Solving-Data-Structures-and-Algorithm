94a592e16decab7a44c2de42f3e371025d45f573
Kayuemkhan <abdulkayuem007@gmail.com>
2024-09-05 10:37:11 +0600

    Browser button added

--- oj_solve/leetcode/Solution3.java
+++ oj_solve/leetcode/Solution3.java
@@ -1,15 +1,25 @@
 package oj_solve.leetcode;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Stack;
+import java.util.Scanner;
 
 class Solution3 {
 
     public static void main(String[] args) {
 
 
+        Scanner scanner = new Scanner(System.in);
 
+        int num,sum =0;
+
+        System.out.println("Enter Any Number : ");
+
+        num = scanner.nextInt();
+        while (num!=0){
+            sum = sum * 10 + (num%10);
+            num = num/ 10;
+        }
+        System.out.println(sum);
+        System.out.println(num);
 
     }
 }
