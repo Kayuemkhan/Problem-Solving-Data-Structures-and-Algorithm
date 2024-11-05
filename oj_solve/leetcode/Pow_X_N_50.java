3ca3d1991a918b36dcee8107f9d24e61b7f480ad
Kayuemkhan <abdulkayuem007@gmail.com>
2024-11-05 14:24:11 +0600

    print_N_added

---
+++ oj_solve/leetcode/Pow_X_N_50.java
@@ -0,0 +1,35 @@
+package oj_solve.leetcode;
+
+public class Pow_X_N_50 {
+
+    public static double myPow(double x, int n) {
+        if (n == 0) return 1.0;
+
+        long exp = Math.abs((long) n);
+        double result = 1.0;
+
+        while (exp > 0) {
+            if ((exp % 2) == 1) {
+                result *= x;
+            }
+            x *= x;
+            exp /= 2;
+        }
+
+        return n < 0 ? 1.0 / result : result;
+    }
+
+    public static void main(String[] args) {
+        double x = 34.00515;
+        int n = -3;
+//        System.out.println(countPow(x,n));
+        System.out.println(myPow(x, n));
+    }
+
+    private static double countPow(double x, int n) {
+        if (n < 2)
+            return x;
+        else
+            return countPow(x + x, n - 1);
+    }
+}
