da9eee262b9a82d2adf9cb0533962d67d7e06e28
abdulkayuem <abdulkayuem007@gmail.com>
2024-11-14 07:47:27 +0600

    print array in reverse array

---
+++ dataStructures/recursion/PrintArrayInReverseOrder.java
@@ -0,0 +1,22 @@
+package dataStructures.recursion;
+
+public class PrintArrayInReverseOrder {
+    public static void main(String[] args) {
+        int[] array = {69 , 87, 45, 21, 47};
+
+        printInReverseOrder(0,array);
+    }
+
+    private static void printInReverseOrder(int index, int[] array) {
+      if(index > array.length-1){
+          return;
+      }
+      else {
+          printInReverseOrder( index+1 , array);
+
+          System.out.println(array[index]);
+
+      }
+
+    }
+}
